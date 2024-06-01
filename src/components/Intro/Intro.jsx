c3adf5549bd3abd7f658d69389c3a7216fda1663
AlanKha <khalilialan@gmail.com>
2024-05-31 22:57:56 -0700

    fixed grammar error in intro

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -5,7 +5,7 @@ export default function Intro() {
         Hi, my name is <span className="text-blue-300 border-b">Alan Khalili</span>👋
       </h1>
       <h2>It's my third year at The University of Tennessee studying Computer Science, and I love coding, coffee, and cooking</h2>
-      <h2>Currently looking for Software Engineering Internshup Opportunities! Feel free to <a href="#Contact" className="underline">reach out</a></h2>
+      <h2>Currently looking for Software Engineering Internship Opportunities! Feel free to <a href="#Contact" className="underline">reach out</a></h2>
     </div>
   );
 }
